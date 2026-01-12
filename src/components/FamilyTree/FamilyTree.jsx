7e3c9b6ac8542e51f45768b6ea4a31a188ea2145
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-01-12 22:03:27 +0600

    added by complex context api

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import GrandPa from './GrandPa';
-import { createContext } from 'react';
- const AssetContext = createContext("");
+import React, { useState } from "react";
+import GrandPa from "./GrandPa";
+import { createContext } from "react";
+const AssetContext = createContext("");
+const MoneyContext = createContext(0);
 
 const FamilyTree = () => {
-    const assets = 'Golden';
-    const newAssets = 'Diamond';
-    return (
-        <div>
-            <h3>Fmaily tree</h3>
-            <div>
-                <AssetContext.Provider value={newAssets}>
-                    <GrandPa assets={assets}></GrandPa>
-                </AssetContext.Provider>
-            </div>
-               
-        </div>
-    );
+  const assets = "Golden";
+  const newAssets = "Diamond";
+  const [money, setMoney] = useState(0);
+
+  return (
+    <div>
+      <h3>Fmaily tree</h3>
+      <h3>Total Money: {money}</h3>
+      <div>
+        <MoneyContext value={[money,setMoney]}>
+          <AssetContext.Provider value={newAssets}>
+            <GrandPa assets={assets}></GrandPa>
+          </AssetContext.Provider>
+        </MoneyContext>
+      </div>
+    </div>
+  );
 };
 
 export default FamilyTree;
 export { AssetContext };
+export { MoneyContext };
